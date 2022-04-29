b2b0cd253b46e69188ec7df0a0673a921dd7a1d0
Putta Srujan <93040995+srujan-here@users.noreply.github.com>
2022-04-29 10:10:32 +0530

    Introductory problems

---
+++ csfi/Intro_Probs/templet/mytem.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define f(a) for (int i=0;i<a;i++)
+#define pb(a) push_back(a)
+#define pf(a) push_front(a)
+
+
+void solve(){
+    
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
